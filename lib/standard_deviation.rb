5414c54b511707c847f6f68c69b3f63c26c5da01
maybemushrooms <thecraftedgem@tuta.io>
2018-08-15 19:56:23 -0600

    testing done

--- lib/standard_deviation.rb
+++ lib/standard_deviation.rb
@@ -3,85 +3,29 @@ require './lib/games'
 
 @ages = [24, 30, 18, 20, 41]
 
-# # Your code here for calculating the standard deviation
+# Your code here for calculating the standard deviation
 
-#   def average_age
-#     @ages.sum.to_f / ages.count
-#   end
-
-#   def standard_deviation_age
-#     sub_ave = @ages.map do |element|
-#     element - average_age
-#     end
-#       rounded_subtracted_average = sub_ave.map do |element|
-#       element.round(1)
-#     end
-#       squared_average = rounded_subtracted_average.map do |element|
-#       element ** 2
-#     end
-#       rounded_squared_average = squared_average.map do |element|
-#         element.round(2)
-#     end
-#       sum_of_squared = rounded_squared_average.sum
-#       bingo = sum_of_squared / @ages.count.to_i
-#       answer = Math.sqrt(bingo)
-#       print answer.round(2)
-# end
-
-
-class Event
-  attr_reader :name,
-              :ages
-
-  def initialize(name, ages)
-    @name = name
-    @ages = ages
+  def average_age
+    @ages.sum.to_f / ages.count
   end
 
   def standard_deviation_age
-    sroot = Math.sqrt(squared.sum / squared.count)
-    return sroot.round(2)
-  end
-
-  def max_age
-    ages.max
-  end
-
-  def min_age
-    ages.min
-  end
-
-  def average_age
-    sum = 0.to_f
-    count = ages.count
-      ages.each do |age|
-        sum += age
-      end
-    average = sum / count
-    return average
-  end
-
-  def subtract_average #standard_deviation_age#(ages)
-    subtracted = []
-    ages.each do |age|
-    rounded = age - average_age
-    subtracted << rounded.round(1)
+    sub_ave = @ages.map do |element|
+    element - average_age
     end
-    return subtracted
-  end
-
-  def squared
-    square = []
-    subtract_average.each do |num|
-      round_squared = num ** 2
-      square << round_squared.round(2)
+      rounded_subtracted_average = sub_ave.map do |element|
+      element.round(1)
     end
-    return square
-  end
-
-
-
+      squared_average = rounded_subtracted_average.map do |element|
+      element ** 2
+    end
+      rounded_squared_average = squared_average.map do |element|
+        element.round(2)
+    end
+      sum_of_squared = rounded_squared_average.sum
+      bingo = sum_of_squared / @ages.count.to_i
+      answer = Math.sqrt(bingo)
+      print answer.round(2)
 end
 
-# When you find the standard deviation, print it out
-standard_deviation_age
+
--- test/event_test.rb
+++ test/event_test.rb
@@ -4,9 +4,39 @@ require './lib/games'
 require './lib/event'
 
 class EventTest < Minitest::Test
+  def test_event_exist
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_instance_of Event, curling
+  end
 
-  def test_it_exists
-    event = Event.new("Curling", [24, 30, 18, 20, 41])
-    assert_instance_of Event, event
+  def test_event_has_name
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal "Curling", curling.name
+  end
+
+  def test_event_has_ages
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal [24, 30, 18, 20, 41], curling.ages
+  end
+
+  def test_can_find_max_age
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 41, curling.max_age
+  end
+
+  def test_can_find_min_age
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 18, curling.min_age
+  end
+
+  def test_can_find_average_age
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 26.6, curling.average_age
+  end
+
+
+  def test_can_find_standard_deviation_age
+    curling = Event.new("Curling", [24, 30, 18, 20, 41])
+    assert_equal 8.28, curling.standard_deviation_age
   end
 end
